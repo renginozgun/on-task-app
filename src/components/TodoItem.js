d239072a9b2609572419476f856f3f2997ca9e2c
Rengin Özgün Akbor <renginozgun@gmail.com>
2021-08-09 16:17:12 +0300

    initial

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TodoItem.css'
+const TodoItem = ({name,done,id}) => {
+    return (
+        <div className='todoItem'>
+            <p className={done && 'todoItem--done'}>{name}</p>
+        </div>
+    )
+}
+
+export default TodoItem
