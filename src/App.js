2e1ff05d5a54b0861e738220925eea0a953370e4
Rengin Özgün Akbor <renginozgun@gmail.com>
2021-08-12 13:53:01 +0300

    12.08
    +Switch button added to show only completed tasks

--- src/App.js
+++ src/App.js
@@ -4,41 +4,57 @@ import './App.css';
 import TodoItem from './components/TodoItem';
 import { useSelector } from 'react-redux';
 import { selectTodoList } from './features/todoSlice';
+import Switch from '@material-ui/core/Switch';
+import { useState } from 'react';
+import Header from './components/header'
 
-import Header from './components/header';
-import { bindActionCreators } from 'redux';
 
 
 function App() {
   const todoList = useSelector(selectTodoList)
- 
- // error const done=useSelector(isChecked)
-  const state = { date: new Date() }
- 
- 
-  return (
+  var checkValue;
+  // error const done=useSelector(isChecked)
+  const state = { date: new Date() }   
+  
+
+const [isEnabled, setIsEnabled] = useState(false);
+const toggleSwitch = () => { setIsEnabled(previousState => !previousState)}
+
+
+const renderCheckboxes=()=>{
+  {return isEnabled ? todoList.map((item) =>
+
+    item.done &&   <TodoItem key={item.id} name={item.item} done={item.done} id={item.id} />
+
+  )
+
+     :  todoList.map((item) =>
+
+      <TodoItem key={item.id} name={item.item} done={item.done} id={item.id} />
+
+    )}
+
+}
+  return ( 
 
     <div className="App">
-     <Header datestring={state.date.toLocaleDateString()}/ >
-       
-        <div className="app_container">
+      <Header datestring={state.date.toLocaleDateString()} />
 
-          <div className="app_todoContainer">
-           
-              {
-               todoList.map((item) =>
-                <TodoItem key={item.id} name={item.item} done={item.done} id={item.id} />  
-               
-               
-      
-                                 
-              )} 
-            
+      <div className="app_container">
+        <div className="app_todoContainer">{ 
+          <Switch  onChange={toggleSwitch} value={isEnabled} name="gilad" /> }
+          {
+            <div className="todo"> 
 
-          </div>
-          <Input />
-         
-        </div>
+            {renderCheckboxes()}
+
+            </div>
+
+          }</div>
+
+        <Input />
+
+      </div>
 
     </div>
   );
